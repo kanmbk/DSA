278e2168a979dbe550faaeb9369d382d53d4525d
kanmbk <kanmbk@gmail.com>
2025-04-20 20:40:46 -0400

    Added recursive mechanism

--- src/com/mbk/learning/dsa/BinarySearchDemo.java
+++ src/com/mbk/learning/dsa/BinarySearchDemo.java
@@ -4,12 +4,13 @@ public class BinarySearchDemo {
 
     public static void main(String[] args){
         
-        //int[] numArray = {4,9,11,15,17,22};
-        //int searchTarget = 17;
-        int[] numArray = new int[5000];
-        int searchTarget = 4000;
+        int[] numArray = {4,9,11,15,17,22};
+        int searchTarget = 17;
+        //int[] numArray = new int[5000];
+        //int searchTarget = 4000;
 
-        int index = BinarySearch(numArray, searchTarget);
+        //int index = BinarySearch(numArray, searchTarget)
+        int index = binarySearch(numArray, searchTarget, 0, numArray.length - 1);
 
         if(index != -1){
             System.out.println("Element " + searchTarget + " found at index: " + index);
@@ -18,23 +19,36 @@ public class BinarySearchDemo {
         }
     }
 
-    static int BinarySearch(int[] numArray, int searchTarget) {
-        int steps = 0;
-        int left = 0;
-        int right = numArray.length - 1;
+    static int binarySearch(int[] numArray, int searchTarget, int left, int right) {
+        //int steps = 0;
+        //int left = 0;
+        //int right = numArray.length - 1;
 
-        while(left <= right){
-            steps++;
+        // while(left <= right){
+        //     steps++;
+        //     int mid = (left + right) / 2;
+        //     if(numArray[mid] == searchTarget){
+        //         System.out.println("Steps taken by binary search: " + steps);
+        //         return mid;
+        //     }else if (numArray[mid] < searchTarget){
+        //         left = mid +1;
+        //     }else
+        //         right = mid + 1;
+        // }
+
+        // Recursive binary search
+        if(left <= right){
             int mid = (left + right) / 2;
             if(numArray[mid] == searchTarget){
-                System.out.println("Steps taken by binary search: " + steps);
                 return mid;
-            }else if (numArray[mid] < searchTarget){
-                left = mid +1;
-            }else
-                right = mid + 1;
+            }else if(numArray[mid] < searchTarget){
+                return binarySearch(numArray, searchTarget, mid+1, right);
+            }else{
+                return binarySearch(numArray, searchTarget, left, mid-1);
+            }
         }
-        System.out.println("Steps taken by binary search: " + steps);
+
+        //System.out.println("Steps taken by binary search: " + steps);
         return -1;
         
     }
